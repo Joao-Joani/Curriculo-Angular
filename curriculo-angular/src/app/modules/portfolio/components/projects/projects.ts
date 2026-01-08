85641f8838ca5bd9c9d1b1e66ddf7a9e6497e9e7
Joao Joani <jgabriel.joani@gmail.com>
2026-01-08 20:41:02 -0300

    Estruturando Projects

--- curriculo-angular/src/app/modules/portfolio/components/projects/projects.ts
+++ curriculo-angular/src/app/modules/portfolio/components/projects/projects.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
@@ -7,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './projects.scss',
 })
 export class Projects {
-
+  public arrayProjects = signal([
+    {
+      scr: "assets/img/projects/tarefas.png",
+      alt: "Projeto Lista de Tarefas com TypeScript",
+      title: "Lista de Tarefas com TypeScript",
+      with: "100px",
+      height: "100px",
+      description: "Criação de uma lista de tarefas utilizando TypeScript, HTML e CSS. O projeto permite adicionar, remover e marcar tarefas como concluídas, proporcionando uma experiência interativa para o usuário.",
+      link: [
+        {
+          name: "Acessar Projeto",
+          href: "https://joao-joani.github.io/ListaTarefas_TypeScript/",
+        },
+      ],
+    },
+  ]);
 }
