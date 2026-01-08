7cc360b2e53962fb46616c016ab858ea28c5dc95
Joao Joani <jgabriel.joani@gmail.com>
2026-01-08 17:04:28 -0300

    Estruturando Header

--- curriculo-angular/src/app/modules/portfolio/pages/home/home.ts
+++ curriculo-angular/src/app/modules/portfolio/pages/home/home.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
 
+// Component
+import { Header } from '../../components/header/header';
+
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [Header],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
