d10404a5ae6e379099e69693c73db97cbdfb81ef
Joao Joani <jgabriel.joani@gmail.com>
2026-01-08 16:06:11 -0300

    Configurações iniciais do projeto.

---
+++ curriculo-angular/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+// Component Pages
+import { Home } from './modules/portfolio/pages/home/home';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: Home
+    }
+];
